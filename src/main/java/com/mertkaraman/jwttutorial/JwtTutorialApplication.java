d8792b0f0e1ee3de448c77b69a33f4cb2112462b
Mert Karaman <mertkrmn06@gmail.com>
2021-08-12 19:57:04 +0300

    After some authentication and authorization

--- src/main/java/com/mertkaraman/jwttutorial/JwtTutorialApplication.java
+++ src/main/java/com/mertkaraman/jwttutorial/JwtTutorialApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -18,25 +20,30 @@ public class JwtTutorialApplication {
     }
 
     @Bean
-    CommandLineRunner run(UserService userService){
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
         return args -> {
-            userService.saveRole(new Role(null,"ROLE_USER"));
-            userService.saveRole(new Role(null,"ROLE_MANAGER"));
-            userService.saveRole(new Role(null,"ROLE_ADMIN"));
-            userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
-            userService.saveUser(new User(null,"John Travolta", "jhon","1234",new ArrayList<>()));
-            userService.saveUser(new User(null,"Will Smith", "will","1234",new ArrayList<>()));
-            userService.saveUser(new User(null,"Jim Carry", "jim","1234",new ArrayList<>()));
-            userService.saveUser(new User(null,"Arnold Schwarzenegger", "arnold","1234",new ArrayList<>()));
+            userService.saveUser(new User(null, "John Travolta", "jhon", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
 
-            userService.addRoleToUser("jhon","ROLE_USER");
-            userService.addRoleToUser("jhon","ROLE_MANAGER");
-            userService.addRoleToUser("will","ROLE_MANAGER");
-            userService.addRoleToUser("jim","ROLE_ADMIN");
-            userService.addRoleToUser("arnold","ROLE_SUPER_ADMIN");
-            userService.addRoleToUser("arnold","ROLE_ADMIN");
-            userService.addRoleToUser("arnold","ROLE_MANAGER");
+            userService.addRoleToUser("jhon", "ROLE_USER");
+            userService.addRoleToUser("jhon", "ROLE_MANAGER");
+            userService.addRoleToUser("will", "ROLE_MANAGER");
+            userService.addRoleToUser("jim", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_MANAGER");
         };
     }
 
--- src/main/java/com/mertkaraman/jwttutorial/service/UserServiceImpl.java
+++ src/main/java/com/mertkaraman/jwttutorial/service/UserServiceImpl.java
@@ -6,16 +6,22 @@ import com.mertkaraman.jwttutorial.repository.RoleRepository;
 import com.mertkaraman.jwttutorial.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j //for logs
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
@@ -26,6 +32,24 @@ public class UserServiceImpl implements UserService {
         this.roleRepository = roleRepository;
     }
     */
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in database");
+            throw new UsernameNotFoundException("User not found in database");
+        } else {
+            log.info("User found in database {}", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -49,13 +73,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
-        log.info("Fetcing user {} ", username);
+        log.info("Fetching user {} ", username);
         return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
-        log.info("Fetcing all users");
+        log.info("Fetching all users");
         return userRepository.findAll();
     }
+
+
 }
